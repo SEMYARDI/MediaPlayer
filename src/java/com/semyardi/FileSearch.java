9904db1f6e97b24edecfac1710af68565c2dd0f0
DobryiD <dimadob9@gmail.com>
2018-04-12 15:57:52 +0200

    Implementation of FileSearch Algorithm

---
+++ src/java/com/semyardi/FileSearch.java
@@ -0,0 +1,53 @@
+package com.semyardi;
+
+
+import java.io.File;
+
+import java.util.Map;
+
+public class FileSearch {
+
+    public void search(String path, Map<String,String> map) {
+
+
+
+            File file = new File(path);
+            File lf[] = file.listFiles();
+            if(lf==null)
+                return;
+            for (File xf : lf) {
+                if (xf == null) return;
+                else if (!xf.canRead()|| xf.isHidden())
+                    continue;
+                else if (xf.isDirectory())
+                    search(xf.getPath(),map);
+                 if (xf.getName().endsWith(".mp3") || xf.getName().endsWith(".aac")
+                         || xf.getName().endsWith(".wav") || xf.getName().endsWith(".wma") /*& xf.getName().startsWith("")*/) {
+
+                    map.put(xf.getName(),xf.getPath());
+                }
+                // Video formats : often used -> (MPEG-1, MPEG-2, MPEG-4, AVI, MOV, AVCHD, H.264,H.265)
+                // Music formats : often used -> (AAC, MP3, WAV, WMA) other music formats -> (AIFF, ASF, FLAC, ADPCM, DSD, LPCM, OGG)
+            }
+
+
+
+
+
+        /*
+        * FileSearch s=new FileSearch();
+        Map<String,String> map= new TreeMap<>();
+        s.search("C://",map);
+        for (String ster:map.values()) {
+
+            System.out.println(ster);
+        }
+        //another variant
+        for(Map.Entry<String,String> entry:map.entrySet()){
+            System.out.println("Name "+entry.getKey()+" -- "+"Path: "+entry.getValue());
+
+        }*/
+    }
+
+
+}
--- src/test/java/com/semyardi/HelloTest.java
+++ src/test/java/com/semyardi/HelloTest.java
@@ -1,10 +1,25 @@
 package com.semyardi;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 public class HelloTest {
     @Test
     public void contextLoads(){
+        FileSearch s=new FileSearch();
+        Map<String,String> map= new TreeMap<>();
+        s.search("C://",map);
+        /*for (String ster:map.values()) {
 
+            System.out.println(ster);
+        }*/
+        //another variant
+        for(Map.Entry<String,String> entry:map.entrySet()){
+            System.out.println("Name "+entry.getKey()+" -- "+"Path: "+entry.getValue());
+
+        }
     }
 }
