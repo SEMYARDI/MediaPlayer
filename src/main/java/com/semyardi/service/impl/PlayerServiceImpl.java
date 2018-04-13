0eb13af770dc511a9df0ecae73c207b18c2c6feb
KravetsArtem <kravets.artem.s@gmail.com>
2018-04-13 19:27:18 +0200

    Merged dev and kpaba_dev branches, fixed annotations.

--- src/main/java/com/semyardi/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/semyardi/service/impl/PlayerServiceImpl.java
@@ -1,9 +1,10 @@
 package com.semyardi.service.impl;
 
 import com.semyardi.service.IPlayerService;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
-
+@Service
 public class PlayerServiceImpl implements IPlayerService {
     public void play(File file) {
         //implementation
